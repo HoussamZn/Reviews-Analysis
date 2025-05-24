d8cd4bf9e743864c03678ad7fc1a5a0be1283a4f
unknown <houssamzitan2003@gmail.com>
2025-05-24 12:11:45 +0100

    fix err

--- review/train.bat
+++ review/train.bat
@@ -1,2 +1,2 @@
-docker exec -it review-spark-master-1 /opt/bitnami/spark/bin/spark-submit /review/train.py
+docker exec -it review-spark-master-1 /opt/bitnami/spark/bin/spark-submit --master spark://spark-master:7077 /review/train.py
 pause
